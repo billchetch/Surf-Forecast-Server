ace3da30226ca5b07a401b5b64c47f45a69c3bf9
billchetch <bill@chetch.net>
2019-04-04 10:21:57 +0100

    Email test

--- test.php
+++ test.php
@@ -1,42 +1,19 @@
 <?php
 require('_include.php');
+$phplib = _SITESROOT_.'services/lib/php/';
+require($phplib.'phpmailer/class.phpmailer.php');
 
-function convertNumber($val, $round = -1){  //scientific exponent
-		//e.g. 0.1908054709434509E+1
-		$val2return = null;
-		$pos = stripos($val, 'E');
-		if($pos !== false){
-			$n = substr($val, 0, $pos);
-			$x = substr($val, $pos);
-			$ar = null;
-			if(stripos($x,'+') !== false){
-				$ar = explode('+', $x);
-			} elseif(stripos($x,'-') !== false){
-				$ar = explode('-', $x);
-				if(count($ar) == 2)$ar[1] = -$ar[1];
-			}
-			if(!$ar || count($ar) != 2)throw new Exception("Cannot parse exponent $x");
-			$val2return = $n * pow(10, $ar[1]);
-		} else {
-			$val2return = $val;
-		}
-		if($round > 0 && $val2return != null)$val2return = round($val2return, $round);
-		return $val2return;
-	}
+$mail = new PHPMailer();
+$mail->SetLanguage('en', $phplib.'phpmailer/language/');
+
 
 //init logger
 Logger::init($dbh, array('log_name'=>'test', 'log_options'=>Logger::LOG_TO_SCREEN));
 $router = null;
 try{
-	$errors = array();
-	$results = FeedResult::getAlreadyParsed($dbh, Config::get('DELETE_FEED_RESULTS_AFTER_DAYS', 90));
-	foreach($results as $result){
-		echo "Deleting feed result ".$result->id."\n";
-		try{
-			$result->delete();
-		} catch (Exception $e){
-			
-		}
+	//mail()
+	if(!mail('bill@chetch.net', 'testing', 'test', 'From: info@bulan-baru.com')){
+		throw new Exception("Email send failed");
 	}
 	
 } catch (Exception $e){
